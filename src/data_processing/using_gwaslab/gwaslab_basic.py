92fc3223460572cced70052f2ad0b5da98590d5f
trafalmadorian97 <peterfor42@gmail.com>
2025-10-04 17:57:38 -0400

    work on lead variant analysis

--- src/data_processing/using_gwaslab/gwaslab_basic.py
+++ src/data_processing/using_gwaslab/gwaslab_basic.py
@@ -1,10 +1,12 @@
 from pathlib import Path
+from typing import Sequence
 
 import gwaslab as gl
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.figure import Figure
 
+from src.data_types.variant import Variant, df_to_variants
 from src.plotting.save_fig import write_plots_to_dir
 
 
@@ -22,11 +24,79 @@ def load_sumstats(pth: Path):
 CSV_DIR_NAME = "csv_tables"
 PLOT_DIR_NAME = "plots"
 
+LEAD_VARIANT_DIR_NAME = "lead_variants"
+
 
 def plot_manhattan_and_qq(sumstats: gl.Sumstats) -> Figure:
     fig, (ax1, ax2) = plt.subplots(figsize=(24, 16), ncols=2, nrows=1)
     sumstats.plot_mqq(figax=[fig, ax1, ax2], skip=2, cut=20, mode="mqq", scaled=True)
     return fig
+
+
+def plot_manhattan_and_qq_anno(sumstats: gl.Sumstats, sig_level: float) -> Figure:
+    fig, (ax1, ax2) = plt.subplots(figsize=(24, 16), ncols=2, nrows=1)
+    sumstats.plot_mqq(
+        figax=[fig, ax1, ax2],
+        skip=2,
+        cut=20,
+        mode="mqq",
+        scaled=True,
+        anno="GENENAME",
+        sig_level_lead=sig_level,
+    )
+    return fig
+
+
+def plot_regional_around_variant(
+    sumstats: gl.Sumstats, chrom: int, pos: int, buffer: int, output_dir: Path
+) -> None:
+    output_dir.mkdir(parents=True, exist_ok=True)
+    sumstats.plot_mqq(
+        mode="r",
+        skip=2,
+        cut=20,
+        scaled=True,
+        region_grid=True,
+        region=(chrom, max(pos - buffer, 0), pos + buffer),
+        save=str(
+            output_dir / "regional_plot.png",
+        ),
+        save_args={"dpi": 400, "facecolor": "white"},
+    )
+
+
+def plot_regional_around_sig_variants(
+    sumstats: gl.Sumstats,
+    sig_variants: Sequence[Variant],
+    output_dir: Path,
+    buffer: int = 500_000,
+) -> None:
+    for item in sig_variants:
+        print(f"plotting around  {item.id}")
+        plot_regional_around_variant(
+            sumstats=sumstats,
+            chrom=item.chromosome,
+            pos=item.position,
+            buffer=buffer,
+            output_dir=output_dir / item.id_normalized,
+        )
+
+
+# def write_regional_plots_around_sig_variants(
+#         sumstats: gl.Sumstats,
+#         sig_variants:Sequence[Variant],
+#         output_dir: Path,
+#         buffer: int= 500_000,
+# ) -> None:
+#     figs = plot_regional_around_sig_variants(
+#         sumstats=sumstats,
+#         sig_variants=sig_variants,
+#         buffer=buffer,
+#     )
+#     for var, fig in figs.items():
+#         sig_dir = output_dir/LEAD_VARIANT_DIR_NAME/ var
+#         sig_dir.mkdir(parents=True, exist_ok=True)
+#         write_plots_to_dir(sig_dir, {"region_plots": fig})
 
 
 def apply_gwaslab_to_gwas(
@@ -45,10 +115,20 @@ def apply_gwaslab_to_gwas(
     )  # Interestingly, some the gene annotations here are inconsistent with those in the paper. May be they used different databases
     print("Done getting lead variants with gwaslab")
     lead_variants.to_csv(csv_output_dir / "gwaslab_lead_variants.csv", index=False)
-    print("plotting with gwaslab")
+    print("plotting regional plots around significant variants...")
+    plot_regional_around_sig_variants(
+        sumstats=sumstats,
+        sig_variants=df_to_variants(lead_variants),
+        output_dir=root_output_dir / LEAD_VARIANT_DIR_NAME,
+    )
+    print("plotting with gwaslab...")
     figs = {}
     figs["gwaslab_manhattan_qq"] = plot_manhattan_and_qq(
         sumstats=sumstats,
+    )
+
+    figs["gwaslab_manhattan_qq_with_annotations"] = plot_manhattan_and_qq_anno(
+        sumstats=sumstats, sig_level=sig_level
     )
     write_plots_to_dir(path=plot_output_Dir, plots=figs)
     print("done plotting with gwaslab")
