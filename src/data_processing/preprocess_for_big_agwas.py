74ac5a7f436bf70ac327535fa8b5187165fa2986
trafalmadorian97 <peterfor42@gmail.com>
2025-10-07 20:29:06 -0400

    fix formatting

--- src/data_processing/preprocess_for_big_agwas.py
+++ src/data_processing/preprocess_for_big_agwas.py
@@ -5,8 +5,10 @@ import polars
 from src.data_processing.decode_me_constants import (
     DECODE_ME_BETA_COL,
     DECODE_ME_CHROM_COL,
+    DECODE_ME_EA,
+    DECODE_ME_NEA,
     DECODE_ME_POS_COL,
-    DECODE_ME_mLOGP_COL, DECODE_ME_NEA, DECODE_ME_EA,
+    DECODE_ME_mLOGP_COL,
 )
 
 _BIG_A_GWAS_REANAME_MAPPING = {
--- test/unit/data_processing/data_processing_pipeline/test_gwas_lab_pipe.py
+++ test/unit/data_processing/data_processing_pipeline/test_gwas_lab_pipe.py
@@ -5,7 +5,9 @@ import pandas as pd
 
 from src.data_processing.data_processing_pipeline.gwas_lab_pipe import GWASLabPipe
 from src.data_processing.decode_me_constants import DECODE_ME_EA
-from src.data_processing.using_gwaslab.gwaslab_constants import GWASLAB_EFFECT_ALLELE_COL
+from src.data_processing.using_gwaslab.gwaslab_constants import (
+    GWASLAB_EFFECT_ALLELE_COL,
+)
 from test.unit.data_processing.path_constants import DUMMY_DATA_PATH
 
 
