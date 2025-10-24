0c12a5ce81597006ea111e442f713e45f81270d7
trafalmadorian97 <peterfor42@gmail.com>
2025-10-24 16:38:18 -0400

    revert analysis

--- src_new/analysis/decode_me_initial_analysis.py
+++ src_new/analysis/decode_me_initial_analysis.py
@@ -1,6 +1,9 @@
 from src_new.analysis.default_runner import DEFAULT_RUNNER
-from src_new.assets.gwas.systemic_lupus_erythematosus.bentham_et_al_2015.analysis_results.bentham_2015_raw_37_lead_variant_region_plots import (
-    BENTHAM_2105_RAW_LEAD_VARIANT_REGION_PLOTS,
+from src_new.assets.gwas.me_cfs.decode_me.analysis_results.decode_me_gwas_1_manhattan import (
+    DECODE_ME_GWAS_1_MANHATTAN_PLOT,
+)
+from src_new.assets.gwas.me_cfs.decode_me.analysis_results.decode_me_gwas_1_manhattan_and_qq import (
+    DECODE_ME_GWAS_1_MANHATTAN_AND_QQ_PLOT,
 )
 
 
@@ -10,12 +13,7 @@ def run_initial_analysis():
         # [DECODE_ME_GWAS_1_SUMSTATS_MINIMAL_FILTERING],
         # [DECODE_ME_GWAS_1_LEAD_VARIANTS]
         # [DECODE_ME_GWAS_1_REGION_PLOTS, MAGMA_GENE_LOCATION_REFERENCE_DATA_BUILD_38_RAW],
-        # [DECODE_ME_GWAS_1_MANHATTAN_AND_QQ_PLOT, DECODE_ME_GWAS_1_MANHATTAN_PLOT],
-        # [BENTHAM_2015_HARMONIZED_BUILD_38]
-        # [BENTHAM_2015_SUMSTATS_MINIMAL_PROCESSING],
-        # [BENTHAM_2015_SUMSTATS_MINIMAL_PROCESSING_FROM_RAW_BUILD_37]
-        [BENTHAM_2105_RAW_LEAD_VARIANT_REGION_PLOTS]
-        # [BENTHAM_2015_RAW_LEAD_VARIANTS]
+        [DECODE_ME_GWAS_1_MANHATTAN_AND_QQ_PLOT, DECODE_ME_GWAS_1_MANHATTAN_PLOT],
     )
 
 
