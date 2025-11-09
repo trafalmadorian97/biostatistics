4ee1570499552e7307dd7b33d2ed9a441c5abce2
Tralfamadoran97 <peterfor42@gmail.com>
2025-11-08 22:49:58 -0500

    Experiment magma graph (#107)
    - Add a rough graph of the results of running MAGMA on DecodeME
    - Plan to turn into Task later

---
+++ experiments/plot_magma_p_values.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+import numpy as np
+import math
+import plotly.express as px
+import pandas as pd
+
+from src_new.build_system.task.gwaslab.gwaslab_constants import GWASLAB_MLOG10P_COL
+from src_new.util.plotting.save_fig import write_plots_to_dir
+
+
+def plot_p_values():
+    df = pd.read_csv("assets/base_asset_store/gwas/ME_CFS/DecodeME/analysis_results/magma/decode_me_gwas_1_build_37_magma_ensemble_specific_tissue_gene_covar_analysis/gene_set_analysis_output.gsa.out",
+                     header=5, sep="\s+" )
+
+    num_tests = len(df)
+    df[GWASLAB_MLOG10P_COL] = -np.log10(df["P"])
+    print(f"Found {num_tests} tests")
+    thresh = 0.05/num_tests
+    print(f"Bonferroni thresh: {thresh}")
+    trunc_df = df.sort_values(by=['P'], ascending=True).iloc[:20]
+    trunc_df["Significance"] = float("nan")
+    trunc_df["Significance"] = "Significant"
+    trunc_df["Significance"] = trunc_df["Significance"].where(trunc_df["P"]<=thresh, "Not Significant")
+    import pdb; pdb.set_trace()
+    plot = px.bar(trunc_df, x="FULL_NAME", y=GWASLAB_MLOG10P_COL, color="Significance")
+    plots ={"decode_me_gtex_tissue":plot}
+    write_plots_to_dir(
+        Path("output")/"gwas_1"/"magma", plots
+    )
+
+if __name__ == "__main__":
+    plot_p_values()
