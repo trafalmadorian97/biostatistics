78fe910664bcc7239ddc8a5618b274028c226593
Tralfamadoran97 <peterfor42@gmail.com>
2025-11-12 16:34:55 -0500

    MAGMA on IBD data (#121)
    - I ran MAGMA on LIU et al's european IBD dataset.
    - Goal was to understand whether the results of the tissue-specific
    analysis reflect known pathology. This could inform how seriously we
    take results of MAGMA applied to ME/CFS.
    - Results shown in the following graph:
    <img width="1520" height="809" alt="magma_ibd_plot"
    src="https://github.com/user-attachments/assets/75bcdd92-5fbd-4722-a2d7-5a019a04fb31"
    />
    - The association with the colon and small-intestine tissues makes
    sense.
    - Association with lung and whole blood is less obvious. Could be
    spurious.

--- src_new/analysis/ibd_analysis.py
+++ src_new/analysis/ibd_analysis.py
@@ -1,15 +1,11 @@
 from src_new.analysis.runner.default_runner import DEFAULT_RUNNER
-from src_new.assets.gwas.inflammatory_bowel_disease.liu_et_al_2023.processed_gwas_data.liu_et_al_2023_eur_liftover_to_37 import (
-    LIU_ET_AL_2023_IBD_EUR_LIFTOVER_37_SUMSTATS,
+from src_new.assets.gwas.inflammatory_bowel_disease.liu_et_al_2023.processed_gwas_data.analysis.magma.liu_et_al_2023_eur_37_specific_tissue_bar_plot import (
+    LIU_ET_AL_IBD_EUR_37_SPECIFIC_TISSUE_ANALYSIS_BAR_PLOT,
 )
 
 
 def run_initial_analysis():
-    DEFAULT_RUNNER.run(
-        # [LIU_ET_AL_2023_IBD_META],
-        # [LIU_ET_AL_2023_IBD_META_EUR_ONLY]
-        [LIU_ET_AL_2023_IBD_EUR_LIFTOVER_37_SUMSTATS]
-    )
+    DEFAULT_RUNNER.run([LIU_ET_AL_IBD_EUR_37_SPECIFIC_TISSUE_ANALYSIS_BAR_PLOT])
 
 
 if __name__ == "__main__":
