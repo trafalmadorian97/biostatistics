414271f5723ddcf49eb1d44a93be8eba86db9269
Tralfamadoran97 <peterfor42@gmail.com>
2025-11-15 18:15:54 -0500

    add tabulate.  rename files (#148)
    - Add tabulate package. This should allow us to generate
    markdown-formatted tables for inclusion in documentation.
    - Move some documentation files.
    - Start work on documentation of lead variants in DecodeME

---
+++ docs/Analysis/DecodeME/DecodeME_Lead_Variants.md
@@ -0,0 +1,10 @@
+# DecodeME Lead Variants
+
+As an initial analysis step, we apply [GWASLab's procedure for extracting lead variants](https://cloufield.github.io/gwaslab/ExtractLead/) to the DecodeME GWAS 1 data.  This procedure groups together significant genetic variants using a sliding-window approach, then reports the most significant variant in each region.
+
+
+
+## Reproducing this
+To reproduce the above-described analysis, run the [initial DecodeME analysis script][mecfs_bio.analysis.decode_me_initial_analysis]
+
+
--- mecfs_bio/analysis/decode_me_initial_analysis.py
+++ mecfs_bio/analysis/decode_me_initial_analysis.py
@@ -1,25 +1,29 @@
+"""
+Script to run initial analysis on DecodeME data.
+"""
+
 from mecfs_bio.analysis.runner.default_runner import DEFAULT_RUNNER
+from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.decode_me_gwas_1_lead_variants import (
+    DECODE_ME_GWAS_1_LEAD_VARIANTS,
+)
 from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.decode_me_gwas_1_manhattan import (
     DECODE_ME_GWAS_1_MANHATTAN_PLOT,
 )
 from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.decode_me_gwas_1_manhattan_and_qq import (
     DECODE_ME_GWAS_1_MANHATTAN_AND_QQ_PLOT,
 )
-from mecfs_bio.assets.reference_data.linkage_disequilibrium_score_reference_data.extracted.eur_ld_scores_thousand_genome_phase_3_v1_extracted import (
-    THOUSAND_GENOME_EUR_LD_REFERENCE_DATA_V1_EXTRACTED,
-)
 
 
 def run_initial_analysis():
+    """
+    Function to run initial analysis on DecodeME data.  Includes generation of manhattan plot,
+    as well as extraction of lead variants.
+    """
     DEFAULT_RUNNER.run(
-        # [DECODE_ME_GWAS_1_TASK, DECODE_ME_QC_SNPS, DECODE_ME_FILTER_SNPS_GWAS_1_TASK]
-        # [DECODE_ME_GWAS_1_SUMSTATS_MINIMAL_FILTERING],
-        # [DECODE_ME_GWAS_1_LEAD_VARIANTS]
-        # [DECODE_ME_GWAS_1_REGION_PLOTS, MAGMA_GENE_LOCATION_REFERENCE_DATA_BUILD_38_RAW],
         [
             DECODE_ME_GWAS_1_MANHATTAN_AND_QQ_PLOT,
             DECODE_ME_GWAS_1_MANHATTAN_PLOT,
-            THOUSAND_GENOME_EUR_LD_REFERENCE_DATA_V1_EXTRACTED,
+            DECODE_ME_GWAS_1_LEAD_VARIANTS,
         ]
     )
 
