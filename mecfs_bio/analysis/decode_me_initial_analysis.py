98066005dd43f6a9e06fca2e17483f6106ea5422
trafalmadorian97 <peterfor42@gmail.com>
2025-11-15 18:14:17 -0500

    invoke green

--- mecfs_bio/analysis/decode_me_initial_analysis.py
+++ mecfs_bio/analysis/decode_me_initial_analysis.py
@@ -12,9 +12,6 @@ from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.decode_me_gwas_1_manhattan 
 from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.decode_me_gwas_1_manhattan_and_qq import (
     DECODE_ME_GWAS_1_MANHATTAN_AND_QQ_PLOT,
 )
-from mecfs_bio.assets.reference_data.linkage_disequilibrium_score_reference_data.extracted.eur_ld_scores_thousand_genome_phase_3_v1_extracted import (
-    THOUSAND_GENOME_EUR_LD_REFERENCE_DATA_V1_EXTRACTED,
-)
 
 
 def run_initial_analysis():
